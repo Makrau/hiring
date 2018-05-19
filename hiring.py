a4761c273b088e8979623135413687809bf8c0b0
Makrau <iago006@hotmail.com>
2018-05-19 20:28:26 -0300

    modularizando funcoes

--- hiring.py
+++ hiring.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 from block_io import BlockIo, BlockIoAPIError
-secretPIN = '35kTalBneckAe'
-def inicializarAPI():
+
+
+def inicializarAPI(_secretPIN):
 	#inicializando a API
 	
 	version = 2 # API version
 	
 	bitcointTestnetApiKey = 'dac5-24ba-d04c-19f9'
-	block_io = BlockIo(bitcointTestnetApiKey, secretPIN, version)
+	block_io = BlockIo(bitcointTestnetApiKey, _secretPIN, version)
 	return block_io
 
-def main():
-	block_io = inicializarAPI()
-
+def send_btc(_block_io, _secretPIN):
 	exampleWallet = '2N2MPfFRUmipSSdFaio23YiYw8eq6SV49qt'
 	testWallet = '2MtSQLgfWpREsDjpax9k71MjiN6GRECuv2P'
 	hiringWallet = 'mnYoahiweETgdXsfY92GCWA6HoRj9knQUw'
 
 	try:
 		#envia 1 btc
-		withdraw = block_io.withdraw_from_addresses(amounts='1.0', 
-			from_addresses=exampleWallet, to_addresses=hiringWallet, pin=secretPIN)
+		amount = '1.0'
+		withdraw = _block_io.withdraw_from_addresses(amounts=amount, 
+			from_addresses=exampleWallet, to_addresses=hiringWallet, pin=_secretPIN)
 
 		print("Status da operacao: ", withdraw['status'])
 		print("Enviado: ", withdraw['data']['amount_sent'])
@@ -28,6 +28,14 @@ def main():
 		print(error)
 
 	return
+
+def main():
+	secretPIN = '35kTalBneckAe'
+	block_io = inicializarAPI(secretPIN)
+	send_btc(block_io, secretPIN)
+
+	return
+
 
 if __name__ == '__main__':
 	main()
